59ed253cbbb30dd66eb354f11a864324a88335f3
Raul Chauvin <raul.chauvin.ojeda@gmail.com>
2023-06-15 15:31:42 -0500

    Imprimir valores en pantalla

---
+++ 01_print.py
@@ -0,0 +1,15 @@
+# Comentario de una linea
+'''
+Comentario
+de 
+varias
+lineas
+'''
+print("Hola, este es mi primer script en Python desde el archivo 01_print")
+
+print("Hola Soy raul y tengo 42 años")
+
+print(12 + 5)
+print(6 - 2)
+print(2 + 3)
+print(8 / 4)
