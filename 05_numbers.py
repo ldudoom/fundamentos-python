f060d7963fd63d171fa747ba3ba661aeaa715a7e
Raul Chauvin <raul.chauvin.ojeda@gmail.com>
2023-06-15 16:03:07 -0500

    Manejo de numeros

---
+++ 05_numbers.py
@@ -0,0 +1,21 @@
+lives = 3
+print(lives)
+print(type(lives))
+
+age = 12
+budget = 100
+
+temperature = 21.12
+print(type(temperature))
+
+lives = lives - 1
+print(lives)
+
+lives += 1
+print(lives)
+
+bigNumber = 420000000000000000000000000000000.1
+print(bigNumber)
+
+smallNumber = 0.000000000000000000000000000123
+print(smallNumber)
