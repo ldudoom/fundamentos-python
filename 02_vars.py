269e0c7dfc600081c6860a34d811fceb800154fa
Raul Chauvin <raul.chauvin.ojeda@gmail.com>
2023-06-15 15:32:01 -0500

    Manejo de variables

---
+++ 02_vars.py
@@ -0,0 +1,11 @@
+print("Hola desde 02 vars")
+
+myName = "Raul Alejandro"
+
+#print("Hola ", myName, " como estas")
+print("Hola " + myName + " como estas")
+
+# Pedir valor por teclado
+myName = input("¿Cuál es tu nombre?: ")
+
+print("Hola " + myName + " como estas, te saludamos desde este script de Python en Replit")
