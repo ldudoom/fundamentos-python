08edf9e58b166560e7c41ed7f8060ab0d514c85a
Raul Chauvin <raul.chauvin.ojeda@gmail.com>
2023-06-15 15:32:14 -0500

    Tipos de datos

---
+++ 03_types.py
@@ -0,0 +1,20 @@
+# string
+my_name = "Raul"
+my_name = 'Alejandro'
+print('my_name =>', my_name)
+print(type(my_name))
+
+# integer
+my_age = 42
+print('my_age =>', my_age)
+print(type(my_age))
+
+# boolean
+is_single = False
+print('is_single =>', is_single)
+print(type(is_single))
+
+#inputs
+my_age = input("Que edad tienes?: ")
+print('my_age =>', my_age)
+print(type(my_age))
