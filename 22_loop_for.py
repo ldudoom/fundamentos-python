3071cf745e498fef304f3f8b4d323bee9464b94d
Raul Chauvin <raul.chauvin.ojeda@gmail.com>
2023-06-19 17:28:39 -0500

    Bucles anidados

--- 22_loop_for.py
+++ 22_loop_for.py
@@ -56,3 +56,17 @@ family = [
 
 for person in family:
     print('Hola', person['name'], person['lastname'])
+
+
+matriz = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+
+print(matriz[0])
+
+for fila in matriz:
+    print(fila)
+    for columna in fila:
+        print(columna)
