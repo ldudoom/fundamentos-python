058b5709e6b0163dc1af17e3b3126debde5dfcf6
Raul Chauvin <raul.chauvin.ojeda@gmail.com>
2023-06-19 11:24:11 -0500

    Estructura de control if

---
+++ 13_if.py
@@ -0,0 +1,14 @@
+if True:
+    print('Deberia ejecutarse')
+
+if False:
+    print('Nunca se ejecuta')
+
+pet = input('Cuál es tu mascota favorita? ')
+
+if pet == 'perro':
+    print('Genial, tienes buen gusto')
+elif pet == 'gato':
+    print('Wow eres muy inteligente')
+else:
+    print('Tienes un', pet)
