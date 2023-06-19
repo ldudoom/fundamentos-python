8e856c9392de8f68c40430180e6dc397809caa79
Raul Chauvin <raul.chauvin.ojeda@gmail.com>
2023-06-19 15:59:47 -0500

    Mejorando un poco el juego con tuplas

--- main.py
+++ main.py
@@ -1,39 +1,48 @@
 # Juego de piedra papel o tijera
 import random
 
-user_option = input('piedra, papel o tijera? => ').lower().strip()
 options = ('piedra', 'papel', 'tijera')
-rand_option = random.randint(0,2)
-'''
-if rand_option == 0:
-    computer_option = 'piedra'
-elif rand_option == 1:
-    computer_option = 'papel'
-else:
-    computer_option = 'tijera' 
-'''
-computer_option = options[rand_option]
-print('Computer option: ', computer_option)
-print('*' * 100)
+user_option = input('piedra, papel o tijera? => ').lower().strip()
 
-if computer_option == 'piedra':
-    if user_option == 'piedra':
-        print('Empate')
-    elif user_option == 'papel':
-        print('Ganaste !!')
-    elif user_option == 'tijera':
-        print(':( Perdiste')
-elif computer_option == 'papel':
-    if user_option == 'piedra':
-        print(':( Perdiste')
-    elif user_option == 'papel':
-        print(':: Empate ::')
-    elif user_option == 'tijera':
-        print('Ganaste !!')
+if not user_option in options:
+    print('Esa opcion no es valida')
 else:
-    if user_option == 'piedra':
-        print('Ganaste !!')
-    elif user_option == 'papel':
-        print(':( Perdiste')
-    elif user_option == 'tijera':
-        print('Empate')
+    '''
+    rand_option = random.randint(0,2)
+    if rand_option == 0:
+        computer_option = 'piedra'
+    elif rand_option == 1:
+        computer_option = 'papel'
+    else:
+        computer_option = 'tijera' 
+    computer_option = options[rand_option]
+    '''
+
+    computer_option = random.choice(options)
+
+    print('*' * 100)
+    print('Computer option: ', computer_option.upper())
+    print('User option: ', user_option.upper())
+    print('*' * 100)
+
+    if computer_option == 'piedra':
+        if user_option == 'piedra':
+            print('Empate')
+        elif user_option == 'papel':
+            print('Ganaste !!')
+        elif user_option == 'tijera':
+            print(':( Perdiste')
+    elif computer_option == 'papel':
+        if user_option == 'piedra':
+            print(':( Perdiste')
+        elif user_option == 'papel':
+            print(':: Empate ::')
+        elif user_option == 'tijera':
+            print('Ganaste !!')
+    else:
+        if user_option == 'piedra':
+            print('Ganaste !!')
+        elif user_option == 'papel':
+            print(':( Perdiste')
+        elif user_option == 'tijera':
+            print('Empate')
