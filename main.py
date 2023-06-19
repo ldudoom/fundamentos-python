bb0b8d46291216472f3c661ca2c2d68990a2414e
Raul Chauvin <raul.chauvin.ojeda@gmail.com>
2023-06-19 15:43:50 -0500

    Actualizando juego de piedra papel tijera, con tuplas

--- main.py
+++ main.py
@@ -2,13 +2,17 @@
 import random
 
 user_option = input('piedra, papel o tijera? => ').lower().strip()
+options = ('piedra', 'papel', 'tijera')
 rand_option = random.randint(0,2)
+'''
 if rand_option == 0:
     computer_option = 'piedra'
 elif rand_option == 1:
     computer_option = 'papel'
 else:
     computer_option = 'tijera' 
+'''
+computer_option = options[rand_option]
 print('Computer option: ', computer_option)
 print('*' * 100)
 
